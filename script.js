f2a33b37629d3354e6bffa50872a90a2e1a866bc
achieversclub <gunjankumar1132007@gmail.com>
2025-12-06 17:46:28 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('Lead page loaded');
+document.getElementById("leadForm").addEventListener("submit", function (e) {
+e.preventDefault();
+
+
+const name = document.getElementById("name").value;
+const phone = document.getElementById("phone").value;
+
+
+const whatsappNumber = "+917488964774";
+const message = `New Lead Details:%0AName: ${name}%0APhone: ${phone}`;
+
+
+const url = `https://wa.me/${whatsappNumber}?text=${message}`;
+
+
+window.open(url, "_blank");
+});
